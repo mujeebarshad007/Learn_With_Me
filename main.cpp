9cf6e10a7e1acb0604cf02e660c9081f43acc6df
mujeebarshad007 <mujeeb.arshad001@gmail.com>
2025-01-17 10:45:45 +0500

    added files

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <ctime>
+#include <climits>
+using namespace std;
+int main()
+{
+    cout << " Hello Guys " << endl;
+    cout << " Notes are in the repository " << endl;
+    return 0;
+}
